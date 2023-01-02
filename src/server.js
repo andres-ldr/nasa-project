879e5eba58e0d968c6e8c2f96830b6ede140cbd3
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-02 17:28:34 -0600

    add loads planets function

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const PORT = 8000;
 const http = require("http");
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const app = require("./app"); //express middleware
 
 const server = http.createServer(app);
 
-server.listen(PORT);
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT);
+}
+
+startServer();
