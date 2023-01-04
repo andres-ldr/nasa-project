d5f79496e347e535f4b3c77e237c04b20bde6472
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-03 18:44:14 -0600

    edit routes name

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const planetsRouter = require("./routes/planets/planets.router");
-const launchessRouter = require("./routes/launches/launches.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
@@ -16,8 +16,8 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.use(launchessRouter);
+app.use('/planets',planetsRouter);
+app.use('/launches',launchesRouter);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
