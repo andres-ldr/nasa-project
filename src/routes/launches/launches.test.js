02d28337c8f14c5e07bdf35e62060525c88ce123
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-05 17:34:59 -0600

    add launch test

---
+++ src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("should respond with 200 success", () => {});
+  test("should catch missing required properties", () => {});
+  test("should catch invalid dates", () => {});
+});
