52c8ec526e0bc86e7a5b66ad90c9b64bffa262f7
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-05 18:22:45 -0600

    add post launch test

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -11,7 +11,34 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launch", () => {
-  test("should respond with 200 success", () => {});
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithOutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+
+  test("should respond with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithOutDate);
+  });
+
   test("should catch missing required properties", () => {});
   test("should catch invalid dates", () => {});
 });
