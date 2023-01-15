bfa83357263e2f78ae3e5cfdce03a7541182e03b
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-14 19:55:47 -0600

    [edit] use mongo connect

--- src/server.js
+++ src/server.js
@@ -1,23 +1,14 @@
-const mongoose = require("mongoose");
 const http = require("http");
 const { loadPlanetsData } = require("./models/planets.model");
 const app = require("./app"); //express middleware
+const { mongoConnect } = require("../services/mongo");
 const PORT = 8000;
 
-const MONGO_URL =
-  "mongodb+srv://andres:in1NpvaZZPqF15AG@nasacluster.6xxuvxx.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB ready");
-});
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT);
 }
