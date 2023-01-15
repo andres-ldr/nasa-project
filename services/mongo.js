d4d902cc8f36a5624184a6ab393d0f2a624d3741
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-14 19:55:20 -0600

    [add] mongo connect module

---
+++ services/mongo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const MONGO_URL =
+  "mongodb+srv://andres:in1NpvaZZPqF15AG@nasacluster.6xxuvxx.mongodb.net/nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB ready");
+});
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect(params) {
+  await mongoose.connect(MONGO_URL);
+}
+module.exports = { mongoConnect };
