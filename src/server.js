ad29695100f7f3175174dea6b4c221eea78c3638
Andrés <lopez.andresrigoberto@gmail.com>
2023-01-16 18:35:05 -0600

    [add] use load launches function

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ const http = require("http");
 const { loadPlanetsData } = require("./models/planets.model");
 const app = require("./app"); //express middleware
 const { mongoConnect } = require("../services/mongo");
+const { loadLaunchData } = require("./models/launches.model");
 const PORT = 8000;
-
 
 const server = http.createServer(app);
 
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
   server.listen(PORT);
 }
 
